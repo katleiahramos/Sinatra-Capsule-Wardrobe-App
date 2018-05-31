f4af6aa9f5379f78517052aa6b4304c8bcdb8f93
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-31 18:18:45 +0000

    create login route and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,43 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "secret"
   end
 
   get "/" do
     erb :welcome
   end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      redirect "users/#{user.id}"
+    else
+      redirect '/login'
+    end
+
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
+
+
+  get '/pieces/new' do
+    erb :'pieces/create_piece'
+  end
+
+  post '/pieces' do
+
+  end
+
+
+
 
 end
