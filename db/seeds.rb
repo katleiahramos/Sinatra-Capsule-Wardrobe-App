70196acc0643c4b46bb0a1bb29d2414bbe83ac17
Katleiah Ramos <katleiahramos@gmail.com>
2018-06-04 19:37:11 +0000

    change working on add to capsule button

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,3 @@
   sweaters = Category.create(name: "Sweaters")
   dresses = Category.create(name: "Dresses")
   skirts = Category.create(name: "Skirts")
-
-
-
-dress = Piece.create(description: "Grey H&M Dress", price: "15")
-dress.category = dresses
-
-bag = Piece.create(description: "Addidas Bag", price: "40")
-bag.category = bags
-
-kat = User.create(username: "kat", password: "awesome", email: "kat@home.com")
-kat.pieces << dress
-kat.pieces << bag
